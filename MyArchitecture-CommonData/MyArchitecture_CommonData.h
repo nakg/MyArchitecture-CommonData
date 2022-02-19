78f58f39995e7b3e657279fab4434bb3b29bd255
김낙진 <nakjin@mail.shinhan.dev>
2022-02-19 22:46:07 +0900

    Apply gitignore

---
+++ MyArchitecture-CommonData/MyArchitecture_CommonData.h
@@ -0,0 +1,18 @@
+//
+//  MyArchitecture_CommonData.h
+//  MyArchitecture-CommonData
+//
+//  Created by 60067670 on 2022/02/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MyArchitecture_CommonData.
+FOUNDATION_EXPORT double MyArchitecture_CommonDataVersionNumber;
+
+//! Project version string for MyArchitecture_CommonData.
+FOUNDATION_EXPORT const unsigned char MyArchitecture_CommonDataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyArchitecture_CommonData/PublicHeader.h>
+
+
